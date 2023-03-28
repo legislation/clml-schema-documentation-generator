2777cead998d0878f3c777784ac77b15b1dab122
pa-tna <60775940+pa-tna@users.noreply.github.com>
2023-03-28 19:35:41 +0100

    Capturing stderr of oXygen

--- schemaDocAutomator/launchOxygen.sh
+++ schemaDocAutomator/launchOxygen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Generating doc for $1 with settings $2 using oXygen at $3"
-exec "$3/schemaDocumentation.sh" "$1" "$2"
+"$3/schemaDocumentation.sh" "$1" "$2" 2>&1
 echo "Done generating documentation"
