b30e27a2819800fdd3362ff345db108bbc8aea50
pa-tna <60775940+pa-tna@users.noreply.github.com>
2023-03-28 19:45:50 +0100

    Run shell script using bash

--- schemaDocAutomator/launchOxygen.sh
+++ schemaDocAutomator/launchOxygen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Generating doc for $1 with settings $2 using oXygen at $3"
-"$3/schemaDocumentation.sh" "$1" "$2" 2>&1
+bash "$3/schemaDocumentation.sh" "$1" "$2" 2>&1
 echo "Done generating documentation"
